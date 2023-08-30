0f2c04d4860582a9e7d6738b3ea4f92f849a013a
DESKTOP-HB3B4MH\Eileen <yiqianc.14@gmail.com>
2023-08-30 11:50:54 +0800

    v1

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/summer2023/',
+  base: '/summer2023_week1/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
